f50d5495b8f8f26e622e49ebd98a9f7710b3f405
ashukla1 <niceashish.shukla@gmail.com>
2016-01-10 20:28:06 +0530

    Assignment 4

---
+++ Part1/Assignment1/Assignment4.js
@@ -0,0 +1,13 @@
+var person= {
+	name: "Äshish Shukla",
+  age: "32",
+  city: "Noida"
+};
+
+(function(){
+  console.log(person.name);
+  console.log(person.age);
+  console.log(person.city);
+
+}()
+)
